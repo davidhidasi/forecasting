f53542ccc85d9e714b1aff5a091bb6fd73da3be4
davidhidasi <bokonon07@gmail.com>
2020-10-13 22:36:28 +0200

    Add genuine forecasting feature

--- prophet_fcst.py
+++ prophet_fcst.py
@@ -35,17 +35,21 @@ def mae(actual, pred):
     return np.mean(abs(actual - pred));
 
 
-def data_prep(DF, ticker, target = 'Close', regressors = [], horizon = 1, shift = 0, cv_sets = 0):    
+def data_prep(DF, ticker, target = 'Close', regressors = [], horizon = 1, shift = 0, cv_sets = 0, genuine_fcst = False):    
     dict_temp = copy.deepcopy(DF)
     df = pd.DataFrame(dict_temp[ticker]) #[:-1]
     df['ds'] = df.index
-    
+        
     non_target_cols = list([a for a in df.columns if a != 'ds' and a != target]) 
     df = df.reindex(columns=(['ds', target] + non_target_cols))
     df = df.rename(columns={target: "y"})
     
-    
+    regressor_df = pd.DataFrame()
     if len(regressors) > 0:
+        if genuine_fcst == True:
+            for col in regressors:
+                regressor_df[col] = df[col].tail(horizon)
+            
         if shift > 0:
             #regressor_cols = list([a for a in non_target_cols if a not in ('Open','High','Low', 'Volume', 'Date','ret')])
             for col in regressors:
@@ -61,14 +65,14 @@ def data_prep(DF, ticker, target = 'Close', regressors = [], horizon = 1, shift 
         for cv_set in range(1,cv_sets):
             df['cv_set' + str(cv_set)] = df['cv_set0'].shift(-cv_set)
     
-    # if weekends are skipped for in fxdata, make sure that the fcst length covers weekdays
-    days_diff = days_between(min(df[df['cv_set0'] == 'test']['ds']), 
-                             max(df[df['cv_set0'] == 'test']['ds'])) # .strftime('%Y-%m-%d')
-                  
-    if days_diff > horizon:
-        horizon = days_diff;
+#    # if weekends are skipped for in fxdata, make sure that the fcst length covers weekdays
+#    days_diff = days_between(min(df[df['cv_set0'] == 'test']['ds']), 
+#                             max(df[df['cv_set0'] == 'test']['ds'])) # .strftime('%Y-%m-%d')
+#                  
+#    if days_diff > horizon:
+#        horizon = days_diff;
         
-    return(df, horizon);
+    return(df, regressor_df);
     
 
 def prophet_estimate(prophet_training_data, 
@@ -87,7 +91,10 @@ def prophet_estimate(prophet_training_data,
                     tune = True,
                     verbose = False,
                     freq = 'D',
-                    include_hist = False):
+                    include_hist = False,
+                    genuine_fcst = False,
+                    new_data = None,
+                    debug = False):
     m = Prophet(#holidays=holidays, 
                         changepoint_prior_scale=changepoints_pr, 
                         holidays_prior_scale=holiday_pr, 
@@ -111,7 +118,14 @@ def prophet_estimate(prophet_training_data,
                             name = regressor,
                             standardize = 'auto',
                             prior_scale = regressor_pr
-                            )                  
+                            )  
+    
+    if debug == True:
+        import pdb
+        pdb.set_trace()
+    # for genuine fcst 
+    if genuine_fcst == True:
+        training_data = training_data.append(test_data)                
     # Fit model
     fit_model = m.fit(training_data)
     # If there are no regressors, create a future df simply based on dates
@@ -120,47 +134,60 @@ def prophet_estimate(prophet_training_data,
       # 3:4, 4:4
     horizon_ew = horizon
     #print(pd.to_datetime(max(training_data['ds'])).dayofweek)
-    if pd.to_datetime(max(training_data['ds'])).dayofweek >= 3:
-        horizon_ew = 4;
+    if pd.to_datetime(max(training_data['ds'])).dayofweek >= 5-horizon:
+        horizon_ew = horizon + 2;
+    
     
     future = fit_model.make_future_dataframe(periods=horizon_ew, 
                                              freq=freq, #'2 min',
                                              include_history=include_hist)
     future = future[future['ds'].dt.dayofweek < 5]
     
-
     # If regressors are to be used then add regressor columns from test to future df
     if len(regressors) > 0:
-        for regressor in regressors:
-            if include_hist == False:
-                future[regressor] = np.array(test_data[regressor])
-            else:
-                future[regressor] = np.array(
-                        training_data[regressor].append(test_data[regressor], 
-                                     ignore_index=True)
-                        )
     
-    import pdb
-    pdb.set_trace()
+        if genuine_fcst == False:     
+            for regressor in regressors:
+                if include_hist == False:
+                    future[regressor] = np.array(test_data[regressor])
+                else:
+                    future[regressor] = np.array(
+                            training_data[regressor].append(test_data[regressor], 
+                                         ignore_index=True)
+                            )
+        else:
+             for regressor in regressors:
+                if include_hist == False:
+                    future[regressor] = np.array(new_data[regressor])
+                else:
+                    future[regressor] = np.array(
+                            training_data[regressor].append(new_data[regressor], 
+                                         ignore_index=True)
+                            )
 
     fcst = fit_model.predict(future)
-    if include_hist == False:
-        joined_data = pd.merge(fcst, test_data, how='inner', on=['ds'])
+    if genuine_fcst == False:
+        if include_hist == False:
+            joined_data = pd.merge(fcst, test_data, how='inner', on=['ds'])
+        else:
+            joined_data = pd.merge(fcst, training_data.append(test_data, ignore_index=True), how='left', on=['ds'])
+        rep_obj = {'changepoints_pr' : changepoints_pr,
+        'holiday_pr' : holiday_pr, 
+        'seasonality_m' : seasonality_m,
+        'seasonality_pr' : seasonality_pr, 
+        'yearly_seas' : yearly_seas,
+        'weekly_seas' : weekly_seas,
+        'daily_seas' : daily_seas,
+        'regressors' : regressors, #','.join(regressors)
+        'regressor_pr' : regressor_pr,
+        'mape' :np.mean([(abs(x - y)/x) for (x,y) in zip(joined_data.y, joined_data.yhat)]),
+        'mae' :np.mean([abs(x - y) for (x,y) in zip(joined_data.y, joined_data.yhat)]) };
+        if tune == False:
+            rep_obj = joined_data
+        if verbose == True: 
+            print(rep_obj)
     else:
-        joined_data = pd.merge(fcst, training_data.append(test_data), how='left', on=['ds'])
-    rep_obj = {'changepoints_pr' : changepoints_pr,
-    'holiday_pr' : holiday_pr, 
-    'seasonality_m' : seasonality_m,
-    'seasonality_pr' : seasonality_pr, 
-    'yearly_seas' : yearly_seas,
-    'weekly_seas' : weekly_seas,
-    'daily_seas' : daily_seas,
-    'regressors' : regressors, #','.join(regressors)
-    'regressor_pr' : regressor_pr,
-    'mape' :np.mean([(abs(x - y)/x) for (x,y) in zip(joined_data.y, joined_data.yhat)]),
-    'mae' :np.mean([abs(x - y) for (x,y) in zip(joined_data.y, joined_data.yhat)]) };
-    if tune == False:
-        rep_obj = joined_data
-    if verbose == True: 
-        print(rep_obj);
+        rep_obj = pd.merge(fcst, training_data, how='outer', on=['ds'])
+        rep_obj['mape'] = [abs(x - y)/x for (x,y) in zip(rep_obj.y, rep_obj.yhat)]
+        rep_obj['mae'] = [abs(x - y) for (x,y) in zip(rep_obj.y, rep_obj.yhat)]
     return(rep_obj);
